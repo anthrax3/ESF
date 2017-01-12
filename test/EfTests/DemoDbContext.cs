3f9eb0d82b983afb0cadbd6446d4fae997f823e9
Bob Chin <bob.chin@qq.com>
2017-01-12 14:07:11 +0800

    modify nofdev reference way

---
+++ test/EfTests/DemoDbContext.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using Intime.Framework.Repository.EF.UnitTest.Models;
+
+namespace Intime.Framework.Repository.EF.UnitTest
+{
+    public class DemoDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Log> Logs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("EF_Test_User");
+            modelBuilder.Entity<User>()
+                .HasKey(v => v.Id)
+                .Property(v => v.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<User>().HasMany(m => m.Logs).WithRequired(m => m.User).HasForeignKey(m => m.UserId);
+            modelBuilder.Entity<User>().Property(v => v.LastChanged).IsConcurrencyToken().IsRowVersion();
+            
+            modelBuilder.Entity<Log>().ToTable("EF_Test_Log");
+            modelBuilder.Entity<Log>().Property(v => v.UserId).HasColumnName("UserId");
+            modelBuilder.Entity<Log>().HasKey(v => v.Id);
+            modelBuilder.Entity<Log>().Property(v => v.ActionName).HasMaxLength(5);
+            //modelBuilder.Entity<Log>().HasRequired(v => v.User).WithMany(m => m.Logs).HasForeignKey(v => v.UserId);
+        }
+
+        public DemoDbContext(string dbName) : base(dbName) 
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public DemoDbContext() 
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+    }
+
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("EF_Test_User");
+            modelBuilder.Entity<User>()
+                .HasKey(v => v.Id)
+                .Property(v => v.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            modelBuilder.Entity<User>().Property(v => v.LastChanged).IsConcurrencyToken().IsRowVersion();
+        }
+
+        public UserDbContext(string dbName) : base(dbName) 
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public UserDbContext()
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+    }
+
+    public class LogDbContext : DbContext
+    {
+        public DbSet<Log> Logs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Log>().ToTable("EF_Test_Log");
+            modelBuilder.Entity<Log>().Property(v => v.UserId).HasColumnName("UserId");
+            modelBuilder.Entity<Log>().HasKey(v => v.Id);
+            modelBuilder.Entity<Log>().Property(v => v.ActionName).HasMaxLength(50);
+            modelBuilder.Entity<Log>().HasRequired(v => v.User).WithMany().HasForeignKey(v => v.UserId);
+        }
+
+        public LogDbContext(string dbName) : base(dbName) 
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+
+        public LogDbContext()
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+        }
+    }
+}
---
+++ test/EfTests/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Intime.Framework.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Intime.Framework.Repository.EF.UnitTest.Models;
+
+namespace Intime.Framework.Repository.EF.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        const string  DbName = "Intime.Framework.Repository.EF.UnitTest.DemoDbContext";
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            
+        }
+
+        [TestMethod]
+        public void GetEFRepository()
+        {
+        
+        }
+
+
+        [TestMethod]
+        public void EFUnitOfWorkTest() 
+        {
+        
+            var userName = Guid.NewGuid().ToString("N");
+
+            var dbContext = new DemoDbContext(DbName);
+            var context = new EfContext(dbContext);
+
+            using (var uow = new EfUnitOfWork(context))
+            {
+                try
+                {
+                    var userRepo = uow.GetRepository<User>();
+                    var user = new User
+                    {
+                        UserName = userName
+                    };
+                    user.Logs = new List<Log>()
+                    {
+                        new Log
+                        {
+                            //User = user,
+                            ActionName = "too",
+                            ActionTime = DateTime.Now
+                        }
+                    };
+                    //userRepo.Add(user);
+                    userRepo.AddGraph(user,m => m.Logs);
+
+                    //var logRepo = uow.GetRepository<Log>();
+                    //logRepo.Add(new Log
+                    //{
+                    //    User = user,
+                    //    ActionName = "too long",
+                    //    ActionTime = DateTime.Now
+                    //});
+
+                    uow.Commit();
+                }
+                catch (Exception ex)
+                {
+                    Output(ex);
+                }
+            }
+
+            var repo = new EF.Repository<User>(context);
+            var any = repo.Query(m => m.UserName == userName).Any();
+            Assert.IsFalse(any);
+        }
+
+        [TestMethod]
+        public void TestMultiTenant()
+        {
+            var userName = Guid.NewGuid().ToString("N");
+
+            var dbContext = new DemoDbContext(DbName);
+            var tp = new MockTenantProvider();
+            var context = new EfContext(dbContext) {TenantContext = tp};
+            var userRepo = new Repository<User>(context);
+
+            userRepo.Add(new User
+            {
+                UserName = userName
+            });
+
+           var user = userRepo.Query(m => m.UserName == userName).Select().FirstOrDefault();
+            Assert.IsNotNull(user);
+            Assert.AreEqual(tp.TenantId,user.TenantId);
+        }
+
+        static void Output(Exception e)
+        {
+            var ex = e;
+            if (ex == null)
+                return;
+            Console.WriteLine(ex.Message);
+            ex = ex.InnerException;
+            Output(ex);
+        }
+
+    }
+
+    public class MockTenantProvider : ITenantContext
+    {
+        #region Implementation of ITenantContext<out int>
+
+        public int TenantId => 1;
+
+        #endregion
+    }
+}
